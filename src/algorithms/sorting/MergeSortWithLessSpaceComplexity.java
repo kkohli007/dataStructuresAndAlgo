0f8e265279493b07a58e10525c9a620dff2c0637
kkohli007 <kkohli007@gmail.com>
2022-05-03 18:05:05 +0530

    Update MergeSortWithLessSpaceComplexity.java
    add descending order with merge sort

--- src/algorithms/sorting/MergeSortWithLessSpaceComplexity.java
+++ src/algorithms/sorting/MergeSortWithLessSpaceComplexity.java
@@ -4,15 +4,27 @@ import src.common.CommonUtil;
 
 public class MergeSortWithLessSpaceComplexity {
 
-    private static int[] unsortedArray = {20,-15,7,35,55,1,-22,7};
 
     public static void main(String[] args)
     {
+        int[] unsortedArray = {20,-15,7,35,55,1,-22,7};
+
+        System.out.println("-------Ascending order-------");
         System.out.println("UnsortedArray: " + CommonUtil.printMyArray(unsortedArray));
 
         unsortedArray = mergeSort(unsortedArray, 0, unsortedArray.length-1);
 
         System.out.println("SortedArray: " + CommonUtil.printMyArray(unsortedArray));
+
+        int[] unsortedArray2 = {20,-15,7,35,55,1,-22,7};
+        System.out.println("-------Descending order-------");
+
+        System.out.println("UnsortedArray: " + CommonUtil.printMyArray(unsortedArray2));
+
+        unsortedArray2 = mergeSortDescending(unsortedArray2, 0, unsortedArray2.length-1);
+
+        System.out.println("SortedArray: " + CommonUtil.printMyArray(unsortedArray2));
+
     }
 
     private static int[] mergeSort(int[] myArray, int start, int end)
@@ -29,7 +41,7 @@ public class MergeSortWithLessSpaceComplexity {
 
         if (myArray[mid] < myArray[mid+1])
         {
-            System.out.println("Intermediate return: " + CommonUtil.printMyArray(unsortedArray));
+            System.out.println("Intermediate return: " + CommonUtil.printMyArray(myArray));
             return myArray;
         }
 
@@ -67,7 +79,64 @@ public class MergeSortWithLessSpaceComplexity {
             counter++;
         }
 
-        System.out.println("Intermediate after merge: " + CommonUtil.printMyArray(unsortedArray));
+        System.out.println("Intermediate after merge: " + CommonUtil.printMyArray(myArray));
+
+        return myArray;
+    }
+
+    private static int[] mergeSortDescending(int[] myArray, int start, int end)
+    {
+        int mid = start + (end - start)/2;
+
+        if(start==end)
+        {
+            return myArray;
+        }
+        // start:0, mid,0 end: 1
+        mergeSortDescending(myArray, start, mid);
+        mergeSortDescending(myArray, mid+1, end);
+
+        if (myArray[mid] > myArray[mid+1])
+        {
+            System.out.println("Intermediate return: " + CommonUtil.printMyArray(myArray));
+            return myArray;
+        }
+
+        int[] tempArray = new int[end - start + 1];
+        int x = start;
+        int y = mid;
+        for(int i= 0; i<tempArray.length; i++)
+        {
+            if(x>mid)
+            {
+                tempArray[i] = myArray[y+1];
+                y++;
+            }
+            else if(y+1>end)
+            {
+                tempArray[i] = myArray[x];
+                x++;
+            }
+            else if(myArray[x]>=myArray[y+1])
+            {
+                tempArray[i] = myArray[x];
+                x++;
+            }
+            else
+            {
+                tempArray[i] = myArray[y+1];
+                y++;
+            }
+        }
+
+        int counter = 0;
+        for(int i=start; i<=end; i++)
+        {
+            myArray[i] = tempArray[counter];
+            counter++;
+        }
+
+        System.out.println("Intermediate after merge: " + CommonUtil.printMyArray(myArray));
 
         return myArray;
     }
